6cd74650c4237ba8f9358d633ea8b41fae72526a
cerramaximiliano <cerramaximiliano@gmail.com>
2023-09-24 15:36:45 -0300

    commit Cluster Mode

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const express = require('express');
+const cluster = require('cluster')
+const os = require('os')
+const express = require('express')
 const isPrime = require('./is-prime')
-const app = express()
 
-app.get('/', (req, res) => {
-    
-    const primes = []
-    const max = Number(req.query.max) || 1000
-    for (let i = 1; i <= max; i++) {
-        if (isPrime(i)) primes.push(i)
+if (cluster.isMaster) {
+    const cpuCount = os.cpus().length
+    for (let i = 0; i < cpuCount; i++) {
+        cluster.fork()
     }
-    res.json(primes)
+}
+else {
+    const app = express()
+
+    app.get('/', (req, res) => {
+        const primes = []
+        const max = Number(req.query.max) || 1000
+        for (let i = 1; i <= max; i++) {
+            if (isPrime(i)) primes.push(i)
+        }
+        res.json(primes)
+    })
+
+    const port = process.env.PORT || 3030
+
+    app.listen(port)
+
+    console.log('app is running on port', port)
+}
+
+cluster.on('exit', (worker) => {
+    console.log('mayday! mayday! worker', worker.id, ' is no more!')
+    cluster.fork()
 })
-
-app.listen(process.env.PORT || 3030)
-
-console.log('app is running!')
-
-module.exports = app
 
 
 // TEST QUE MUESTRA QUE EL MODO CLUSTER APROVECHA MEJOR EL RENDIMIENTO USANDO TODOS LOS NÚCLEOS
